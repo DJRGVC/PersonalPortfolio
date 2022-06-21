482c7a74056fa3eeb6b6a7655f2dfad7895e728e
Daniel Grant <captaindgrant@gmail.com>
2022-06-21 20:38:46 +0100

    changed spacing/padding

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,8 +7,8 @@ import React from "react";
 const NavBar = () => {
   return (
     <Flex
-      w="100%"
-      h="10%"
+      w="100vw"
+      h="10vh"
       backgroundColor={useColorModeValue("brand.300", "brand.200")}
       position="fixed"
       zIndex={50}
@@ -17,15 +17,10 @@ const NavBar = () => {
       p={8}
       opacity=".9"
     >
-      <Flex
-        margin={4}
-        position="fixed"
-        top="-1%"
-        paddingLeft={{ base: "16vw", md: "30vw", lg: "36vw" }}
-      >
+      <Flex paddingBottom={{ base: "8vw", md: "5vw", lg: "3vw" }}>
         <Hamburger />
       </Flex>
-      <HStack margin={4} zIndex={100} justifyContent="right">
+      <HStack margin={{ base: "0", md: ".75vw", lg: "1.5vw" }}>
         <BackToHome />
         <ColorModeSwitcher />
       </HStack>
